b9452528d7d4e5c31c26a754565f1ce09ba9eb6d
Crow314 <master@crow31415.net>
2023-02-16 15:12:59 +0900

    Create main.go

---
+++ cmd/interpreter/main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	t.Fail()
+}
+
+func TestSub(t *testing.T) {
+	t.Fail()
+}
+
+func TestMul(t *testing.T) {
+	t.Fail()
+}
+
+func TestDiv(t *testing.T) {
+	t.Fail()
+}
